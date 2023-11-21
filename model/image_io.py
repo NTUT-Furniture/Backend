cd267405b0b8484419eb48767915629fc021187c
DF <dragonflybaby2.0@gmail.com>
2023-11-22 03:34:00 +0800

    Add image_io module for image uploading
    This commit introduces a new module, image_io.py, that is used for image uploading. The module has functionality to list files, get directory path, generate a unique file name for uploads, and save the uploaded file in the correct location. The implementation currently supports differentiating between avatar and product uploads. This module will provide the necessary underlying support for any image uploading requirements in the application.

---
+++ model/image_io.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+from fastapi import UploadFile
+
+class ImgSourceEnum(str, Enum):
+    avatar = "avatar"
+    product = "product"
+
+def list_files(dir_path: str) -> list:
+    files_list = [os.path.join(root, file) for root, _, files in os.walk(dir_path) for file in files]
+    return files_list
+
+def get_directory_path(whom: ImgSourceEnum) -> str:
+    path = f"../upload_images/{whom.value}"
+    return path
+
+def get_filename(file: UploadFile) -> str:
+    filename, filetype = file.filename.split(".")
+    new_filename = f"{filename}_{datetime.now().timestamp()}"
+    return f"{new_filename}.{filetype}"
+
+def save_file(file: Optional[UploadFile], whom: ImgSourceEnum) -> bool:
+    try:
+        directory_path = get_directory_path(whom)
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+        filename = get_filename(file)
+
+        with open(os.path.join(directory_path, filename), "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+    except (OSError, shutil.Error) as e:
+        # LOG_ERROR(f"Error copying file: {e}")  TODO: the logger system
+        return False
+    except (FileNotFoundError, PermissionError) as e:
+        # LOG_ERROR(f"Error copying file: {e}")
+        return False
+    return True
