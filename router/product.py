f3aea7c59a22ebe2bfea23d0d15343b2fdaa6b96
kyynk <zhan.kiliinn@gmail.com>
2023-11-30 16:51:18 +0800

    add product

---
+++ router/product.py
@@ -0,0 +1,110 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
+from model.product import ReturnProduct, CreateProductForm, ReturnCreateProduct, UpdateProductForm
+from model.general import SuccessModel, ErrorModel
+
+from utils.db_process import get_all_result, execute_query, dict_to_sql_command, dict_delete_none
+
+import uuid
+
+router = APIRouter()
+
+@router.get("/product", tags=["product"], responses={
+    status.HTTP_200_OK: {
+        "model": ReturnProduct
+    },
+    status.HTTP_404_NOT_FOUND: {
+        "model": ErrorModel
+    }
+})
+async def get_product(
+	shop_uuid: str
+):
+    sql = """
+        SELECT * FROM `Product` WHERE shop_uuid = %s;
+    """
+    result = get_all_result(sql, (shop_uuid,))
+    if result:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=jsonable_encoder({
+                "msg": "Success",
+                "data": result
+            })
+        )
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content=jsonable_encoder({
+            "msg": "Fail"
+        })
+    )
+
+@router.post("/product", tags=["product"], responses={
+    status.HTTP_200_OK: {
+        "model": ReturnCreateProduct
+    },
+    status.HTTP_404_NOT_FOUND: {
+        "model": ErrorModel
+    }
+})
+async def create_product(
+    product_form: CreateProductForm = Depends(CreateProductForm.as_form)
+):
+    product_form = product_form.model_dump()
+    id = str(uuid.uuid4())
+    sql = """
+        INSERT INTO `Product`
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, DEFAULT);
+    """
+    result = execute_query(sql, (id,) + tuple(product_form.values()))
+    if result:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=jsonable_encoder({
+                "msg": "Success",
+                "data": id
+            })
+        )
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content=jsonable_encoder({
+            "msg": "Fail"
+        })
+    )
+
+@router.put("/product", tags=["product"], responses={
+    status.HTTP_200_OK: {
+        "model": SuccessModel
+    },
+    status.HTTP_404_NOT_FOUND: {
+        "model": ErrorModel
+    }
+})
+async def update_product(
+    product_form: UpdateProductForm = Depends(UpdateProductForm.as_form)
+):
+    product_form = product_form.model_dump()
+    product_form = dict_delete_none(product_form)
+    sql_set_text, sql_set_values = dict_to_sql_command(product_form)
+    sql = f"""
+        UPDATE `Product` SET {sql_set_text}
+        WHERE product_uuid = %s;
+    """
+    result = execute_query(sql, (sql_set_values + (product_form["product_uuid"],)))
+    if result:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=jsonable_encoder({
+                "msg": "Success"
+            })
+        )
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content=jsonable_encoder({
+            "msg": "Fail"
+        })
+    )
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from core.settings import Settings
-from router import account, shop
+from router import account, shop, product
 
 app = FastAPI(
     title="NFT API",
@@ -14,3 +14,4 @@ app = FastAPI(
 app.add_middleware(CORSMiddleware)
 app.include_router(account.router, prefix=Settings["api_prefix"])
 app.include_router(shop.router, prefix=Settings["api_prefix"])
+app.include_router(product.router, prefix=Settings["api_prefix"])
