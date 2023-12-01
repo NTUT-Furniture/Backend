2c1fc8e6a85325ffa6b568502e20e465e116ae62
df <dragonflybaby2.0@gmail.com>
2023-12-01 18:07:49 +0800

    Update ImageUploadForm and ImageTypeModel in model/image.py
    The ImageUploadForm and ImageTypeModel in the image.py module have been updated to simplify and improve the image upload and retrieval process. ImageUploadForm, which previously handled image uploads, and ImageGetForm, which dealt with image retrievals, have been removed. The ImageTypeModel enum class has been introduced to differentiate between two types of images - avatars and banners. The error message for ImageIOFail model has been updated to make it clearer. These changes will allow a more flexible and functional interaction with different image types.

--- model/image.py
+++ model/image.py
@@ -1,29 +1,17 @@
 from enum import Enum
 
-from fastapi import UploadFile
 from pydantic import BaseModel
 
 from model.general import SuccessModel
-from utils.as_form import as_form
 
-@as_form
-class ImageUploadForm(BaseModel):
-    owner_uuid: str
-    file: UploadFile
+class ImageTypeModel(str, Enum):
+    avatar = "avatar"
+    banner = "banner"
 
 class ImageUploadSuccessModel(SuccessModel):
     image_uuid: str
     content_type: str
     size: int
 
-class ImageTypeModel(str, Enum):
-    avatar = "avatar"
-    banner = "banner"
-
-@as_form
-class ImageGetForm(BaseModel):
-    owner_uuid: str
-    img_type: ImageTypeModel = ImageTypeModel.avatar
-
 class ImageIOFailModel(BaseModel):
-    msg: str = ""
+    msg: str = "Failed to upload image!"
