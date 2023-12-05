503153faefc41b0fd99f3c016f6f2f64726e15de
DF <dragonflybaby2.0@gmail.com>
2023-12-05 18:30:58 +0800

    Refactor image handling in API router
    This commit significantly streamlines how images are handled in the API router. Previously, the logic related to image type validation and existence of owner in database was repeated. Now, separate functions have been created for these, reducing the complexity of the `upload_image` and `get_image` functions. Additionally, the `jsonable_encoder` has been removed as it was adding unnecessary complexity to JSONResponse, with dict proving to be sufficient. This refactor leads to improved readability and maintainability of the code.

--- router/image.py
+++ router/image.py
@@ -1,7 +1,6 @@
 from typing import Annotated
 
 from fastapi import APIRouter, status, UploadFile, File, Depends
-from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 from model.account import Account
@@ -13,106 +12,55 @@ router = APIRouter(
     tags=["image", "product", "account", "shop"],
 )
 
+async def handle_image_not_found(img_type, owner_uuid):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={
+            "msg": f"Image type must be avatar or banner! Get {img_type} instead!"
+        }
+    )
+
+async def is_image_available(account_uuid):
+    return await if_exists_in_db("Account", "account_uuid", account_uuid) or \
+        await if_exists_in_db("Product", "product_uuid", account_uuid) or \
+        await if_exists_in_db("Shop", "shop_uuid", account_uuid)
+
 @router.post(
-    "/upload",
-    description="Upload image(jpeg/png) for product, max_size = 10MB",
-    responses={
-        status.HTTP_200_OK: {
-            "model": ImageUploadSuccessModel
-        },
-        status.HTTP_400_BAD_REQUEST: {
-            "model": ImageIOFailModel
-        },
-    },
-    tags=["upload"]
+    "/upload", responses={
+        status.HTTP_200_OK: {"model": ImageUploadSuccessModel},
+        status.HTTP_400_BAD_REQUEST: {"model": ImageIOFailModel}
+    }
 )
 async def upload_image(
-        account: Annotated[
-            Account,
-            Depends(auth.get_current_active_user)],
-        img_type: ImageTypeModel = ImageTypeModel.avatar,
-        file: UploadFile = File(...)
+        account: Annotated[Account, Depends(auth.get_current_active_user)],
+        img_type: ImageTypeModel = ImageTypeModel.avatar, file: UploadFile = File(...)
 ):
     owner_uuid = account.account_uuid
     if img_type == ImageTypeModel.banner:
         if not await if_exists_in_db("Shop", "account_uuid", owner_uuid):
-            return JSONResponse(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                content=jsonable_encoder(
-                    {
-                        "msg": f"No such shop: {owner_uuid}"
-                    }
-                )
-            )
+            return {"msg": f"No such shop: {owner_uuid}"}
         return await image_io.save_file(file, owner_uuid, img_type)
-    elif img_type == ImageTypeModel.avatar:
-        if await if_exists_in_db("Account", "account_uuid", owner_uuid) or \
-                await if_exists_in_db("Product", "product_uuid", owner_uuid) or \
-                await if_exists_in_db("Shop", "shop_uuid", owner_uuid):
-            return await image_io.save_file(file, owner_uuid, img_type)
-    else:
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content=jsonable_encoder(
-                {
-                    "msg": f"Image type must be avatar or banner! Get {img_type} instead!"
-                }
-            )
-        )
-    return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        content=jsonable_encoder(
-            {
-                "msg": f"No such owner: {owner_uuid}"
-            }
-        )
-    )
+    elif img_type == ImageTypeModel.avatar and await is_image_available(owner_uuid):
+        return await image_io.save_file(file, owner_uuid, img_type)
+    return handle_image_not_found(img_type, owner_uuid)
 
 @router.get(
-    "/get_image/",
-    description="Get image(jpeg/png) for account or product",
-    responses={
-        status.HTTP_200_OK: {
-            "model": ImageUploadSuccessModel
-        },
-        status.HTTP_400_BAD_REQUEST: {
-            "model": ImageIOFailModel
-        },
-        status.HTTP_404_NOT_FOUND: {
-            "model": ImageIOFailModel
-        },
-    },
-    tags=["get"]
+    "/get_image/", responses={
+        status.HTTP_200_OK: {"model": ImageUploadSuccessModel},
+        status.HTTP_400_BAD_REQUEST: {"model": ImageIOFailModel}
+    }
 )
 async def get_image(
         account: Annotated[
-            Account,
-            Depends(auth.get_current_active_user)],
+            Account, Depends(auth.get_current_active_user)],
         img_type: ImageTypeModel = ImageTypeModel.avatar
 ):
     owner_uuid = account.account_uuid
     if img_type == ImageTypeModel.banner:
         if not await if_exists_in_db("Shop", "shop_uuid", owner_uuid):
-            return JSONResponse(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                content=jsonable_encoder(
-                    {
-                        "msg": f"No such shop: {owner_uuid}"
-                    }
-                )
-            )
+            return {"msg": f"No such shop: {owner_uuid}"}
         return await image_io.get_file(owner_uuid, img_type)
-    elif img_type == ImageTypeModel.avatar:
-        if await if_exists_in_db("Account", "account_uuid", owner_uuid) or \
-                await if_exists_in_db("Product", "product_uuid", owner_uuid) or \
-                await if_exists_in_db("Shop", "shop_uuid", owner_uuid):
-            return await image_io.get_file(owner_uuid, img_type)
+    elif img_type == ImageTypeModel.avatar and await is_image_available(owner_uuid):
+        return await image_io.get_file(owner_uuid, img_type)
     else:
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content=jsonable_encoder(
-                {
-                    "msg": f"Image type must be avatar or banner! Get {img_type} instead!"
-                }
-            )
-        )
+        return handle_image_not_found(img_type, owner_uuid)
