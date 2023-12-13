08afb85b33546129f2a1227eebd2801164fd95f3
kyynk <zhan.kiliinn@gmail.com>
2023-12-13 20:34:04 +0800

    add subscription

---
+++ model/subscription.py
@@ -0,0 +1,21 @@
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+from model.general import SuccessModel
+from utils.as_form import as_form
+
+class Subscription(BaseModel):
+    account_uuid: str
+    shop_uuid: str
+
+class ReturnProduct(SuccessModel):
+    data: List[Subscription]
+
+@as_form
+class CreateSubscriptionForm(BaseModel):
+    account_uuid: str
+    shop_uuid: str
+
+class ReturnCreateSubscription(SuccessModel):
+    data: str
