d5979a33134d0124ce619ec30dec325a30687280
DF <dragonflybaby2.0@gmail.com>
2023-12-16 10:18:43 +0800

    Check if current user is deactivated

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -109,13 +109,11 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> Acc
 async def get_current_active_user(
         current_user: Annotated[Account, Depends(get_current_user)]
 ):
-
-    # TODO: Add disabled column to Account table
-    # if current_user.disabled:
-    #     raise HTTPException(
-    #         status_code=400,
-    #         detail="Inactive user"
-    #     )
+    if current_user.is_active != 1:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Inactive user"
+        )
     return current_user
 
 async def if_account_owns_shop(
