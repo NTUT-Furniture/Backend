9f58b6caeb738c3b6923b481e4547538646eb273
kyynk <zhan.kiliinn@gmail.com>
2023-12-17 13:54:20 +0800

    add subscription data

--- sql-scripts/3Subscription.sql
+++ sql-scripts/3Subscription.sql
@@ -13,7 +13,11 @@ INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('831cd0d0-8916-11
 INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('831cd0d0-8916-11ee-bb92-42010aae0002', 'f150acb3-8998-11ee-bb92-42010aae0002');
 INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('831cdb41-8916-11ee-bb92-42010aae0002', 'f150af72-8998-11ee-bb92-42010aae0002');
 INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('831cdb41-8916-11ee-bb92-42010aae0002', 'f150b096-8998-11ee-bb92-42010aae0002');
-INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('831cdd39-8916-11ee-bb92-42010aae0002', 'f150b186-8998-11ee-bb92-42010aae0002');
+INSERT INTO NFT.Subscriptio1n (account_uuid, shop_uuid) VALUES ('831cdd39-8916-11ee-bb92-42010aae0002', 'f150b186-8998-11ee-bb92-42010aae0002');
 INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('831cdd39-8916-11ee-bb92-42010aae0002', 'f150b368-8998-11ee-bb92-42010aae0002');
 INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('831cde2e-8916-11ee-bb92-42010aae0002', 'f150b459-8998-11ee-bb92-42010aae0002');
 INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('831cde2e-8916-11ee-bb92-42010aae0002', 'f150b531-8998-11ee-bb92-42010aae0002');
+INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('849c3d7c-8916-11ee-bb92-42010aae0002', 'c4f8d1b0-8d24-11ee-bb92-42010aae0002');
+INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('849c38d1-8916-11ee-bb92-42010aae0002', 'c4f8d1b0-8d24-11ee-bb92-42010aae0002');
+INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('76eafe7a-8d1d-11ee-bb92-42010aae0002', '6f54c6e7-51f2-4582-ba33-6223a7cf58ca');
+INSERT INTO NFT.Subscription (account_uuid, shop_uuid) VALUES ('831cdd39-8916-11ee-bb92-42010aae0002', '6f54c6e7-51f2-4582-ba33-6223a7cf58ca');
