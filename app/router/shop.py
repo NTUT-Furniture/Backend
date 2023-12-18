b6c6f18abcdc4bc685f5b448902626ab83f683d2
df <dragonflybaby2.0@gmail.com>
2023-12-18 22:33:07 +0800

    Update Shop model and responses

--- app/router/shop.py
+++ app/router/shop.py
@@ -1,13 +1,13 @@
 import uuid
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.responses import JSONResponse
 
 from app.model.account import Account
-from app.model.general import SuccessModel, ErrorModel
+from app.model.general import ErrorModel
 from app.model.shop import (
-    ReturnShop, CreateShopForm, ReturnCreateShop, UpdateShopForm, Shop,
+    CreateShopForm, UpdateShopForm, Shop,
 )
 from app.utils import auth
 from app.utils.db_process import (
@@ -22,7 +22,7 @@ router = APIRouter(
     "/", tags=["get"],
     responses={
         status.HTTP_200_OK: {
-            "model": ReturnShop
+            "model": Shop
         },
         status.HTTP_404_NOT_FOUND: {
             "model": ErrorModel
@@ -36,21 +36,22 @@ async def get_shop(account_uuid: str):
     if result:
         shop = result[0]
         shop["update_time"] = str(shop["update_time"])
-        return ReturnShop(data=[Shop(**shop)])
+        return Shop(**shop)
 
-    return JSONResponse(
+    raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        content={
-            "msg": f"Shop with account_uuid {account_uuid} not found"
-        }
+        detail=ErrorModel(msg=f"Shop with account_uuid {account_uuid} not found")
     )
 
 @router.post(
     "/",
     tags=["create"],
     responses={
-        status.HTTP_200_OK: {
-            "model": ReturnCreateShop
+        status.HTTP_201_CREATED: {
+            "model": Shop
+        },
+        status.HTTP_400_BAD_REQUEST: {
+            "model": ErrorModel
         },
         status.HTTP_404_NOT_FOUND: {
             "model": ErrorModel
@@ -61,10 +62,10 @@ async def create_shop(
         account: Annotated[Account, Depends(auth.get_current_active_user)],
         shop_form: CreateShopForm = Depends(CreateShopForm.as_form)
 ):
-    if if_exists_in_db("Shop", "account_uuid", account.account_uuid):
-        return JSONResponse(
-            content=f"The account {account.account_uuid} already owned a shop",
-            status_code=status.HTTP_400_BAD_REQUEST
+    if await if_exists_in_db("Shop", "account_uuid", account.account_uuid):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=ErrorModel(msg=f"Shop with account_uuid {account.account_uuid} already exists")
         )
     shop_id = str(uuid.uuid4())
     shop_form = shop_form.model_dump()
@@ -72,17 +73,14 @@ async def create_shop(
 
     result = execute_query(sql, (shop_id, account.account_uuid) + tuple(shop_form.values()))
     if result:
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={
-                "data": result
-            }
+        return Shop(
+            shop_uuid=shop_id,
+            account_uuid=account.account_uuid,
+            **shop_form
         )
-    return JSONResponse(
+    raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
-        content={
-            "msg": "Something went wrong."
-        }
+        detail=ErrorModel(msg=f"Something went wrong")
     )
 
 @router.put(
@@ -90,7 +88,7 @@ async def create_shop(
     tags=["update"],
     responses={
         status.HTTP_200_OK: {
-            "model": SuccessModel
+            "model": UpdateShopForm
         },
         status.HTTP_404_NOT_FOUND: {
             "model": ErrorModel
@@ -109,13 +107,9 @@ async def update_shop(
     if result:
         return JSONResponse(
             status_code=status.HTTP_200_OK,
-            content={
-                "msg": "Shop updated successfully"
-            }
+            content=UpdateShopForm(**shop_form)
         )
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
-        content={
-            "msg": "Something went wrong."
-        }
+        content=ErrorModel(msg=f"Something went wrong")
     )
