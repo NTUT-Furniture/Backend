138fb3c22894686ce923a0aac0cb143642c70002
df <dragonflybaby2.0@gmail.com>
2023-12-18 19:42:56 +0800

    Update Account model

--- app/model/account.py
+++ app/model/account.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from pydantic import BaseModel
 
 from app.utils.as_form import as_form
@@ -8,12 +6,12 @@ class Account(BaseModel):
     account_uuid: str
     name: str
     email: str
-    phone: Optional[str]
-    birthday: Optional[str]
-    address: Optional[str]
-    is_active: int
-    role: int
-    update_time: str
+    phone: str | None = None
+    birthday: str | None = None
+    address: str | None = None
+    is_active: int | None = 1
+    role: int | None = 0
+    update_time: str | None = None
 
 class AccountList(BaseModel):
     accounts: list[Account]
@@ -23,18 +21,12 @@ class CreateAccountForm(BaseModel):
     name: str
     email: str
     pwd: str
-    phone: Optional[str] = None
-    credit_card: Optional[str] = None
-    birthday: Optional[str] = None
-    address: Optional[str] = None
+    phone: str | None = None
+    credit_card: str | None = None
+    birthday: str | None = None
+    address: str | None = None
 
 @as_form
-class UpdateAccountForm(BaseModel):
-    name: Optional[str]
-    email: Optional[str]
-    pwd: Optional[str]
-    phone: Optional[str]
-    credit_card: Optional[str]
-    birthday: Optional[str]
-    address: Optional[str]
-    is_active: Optional[bool]
+class UpdateAccountForm(CreateAccountForm):
+    is_active: int | None = None
+    role: int | None = None
