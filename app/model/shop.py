ed9c88ebbaf59e5ea45de7c03a342289f49d4197
df <dragonflybaby2.0@gmail.com>
2023-12-18 22:33:07 +0800

    Update Shop model and responses

--- app/model/shop.py
+++ app/model/shop.py
@@ -20,6 +20,13 @@ class CreateShopForm(BaseModel):
     name: str
     description: str | None = None
 
+class CreateShopResponse(CreateShopForm):
+    shop_uuid: str
+    account_uuid: str
+
 @as_form
 class UpdateShopForm(CreateShopForm):
+    shop_uuid: str | None = None
+    name: str | None = None
+    description: str | None = None
     is_active: int | None = None
