2d0fc62d82dc2ed74416c9a7e2710d5c621e1dfb
DF <dragonflybaby2.0@gmail.com>
2023-12-20 02:16:18 +0800

    Put routes back

--- app/router/__init__.py
+++ app/router/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
 from .account import router as account_router
-# from .image import router as image_router
-# from .coupon import router as coupon_router
+from .image import router as image_router
+from .coupon import router as coupon_router
 from .login import router as login_router
 from .product import router as product_router
 from .shop import router as shop_router
@@ -12,8 +12,8 @@ def register_router(app):
     router.include_router(account_router, prefix="/account")
     router.include_router(shop_router, prefix="/shop")
     router.include_router(product_router, prefix="/product")
-    # router.include_router(image_router, prefix="/image")
-    # router.include_router(coupon_router, prefix="/coupon")
+    router.include_router(image_router, prefix="/image")
+    router.include_router(coupon_router, prefix="/coupon")
     router.include_router(login_router)
 
     app.include_router(router, prefix="/api")
