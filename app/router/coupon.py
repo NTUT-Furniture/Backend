73a7f3df0b6e7c6960ea34564b9af88590533297
DF <dragonflybaby2.0@gmail.com>
2023-12-20 02:22:10 +0800

    Add put method to /api/coupon

--- app/router/coupon.py
+++ app/router/coupon.py
@@ -82,3 +82,52 @@ async def create_coupon(
         status_code=status.HTTP_400_BAD_REQUEST,
         detail=f"Something wrong happened."
     )
+
+@router.put(
+    "/", tags=["update"], responses={
+        status.HTTP_200_OK: {
+            "model": Coupon
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "model": ErrorModel
+        }
+    }
+)
+async def update_coupon(
+        account: Annotated[Account, Depends(auth.get_current_active_user)],
+        shop_uuid: str | None = None,
+        coupon_form: CreateCouponForm = Depends(CreateCouponForm.as_form),
+):
+    if shop_uuid:
+        if account.role != 1:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"Only admin can update coupon for other shops."
+            )
+    else:
+        shop_uuid = await get_shop_by_account_uuid(account_uuid=account.account_uuid)
+    coupon_form = coupon_form.model_dump()
+    sql = """
+    SELECT (COUNT(*) = 0) AS not_exist FROM `Coupon` WHERE shop_uuid = %s AND coupon_code = %s;
+    """
+    result = get_all_results(sql, (shop_uuid, coupon_form["coupon_code"]))
+    if result[0]["not_exist"]:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Coupon code {coupon_form['coupon_code']} under shop {shop_uuid} doesn't exist."
+        )
+
+    sql = """
+        UPDATE `Coupon`
+        SET discount = %s, expire_time = %s
+        WHERE shop_uuid = %s AND coupon_code = %s;
+    """
+    result = execute_query(
+        sql, (coupon_form["discount"], coupon_form["expire_time"], shop_uuid, coupon_form["coupon_code"])
+    )
+    if result:
+        return Coupon(shop_uuid=shop_uuid, update_time=datetime.datetime.now(), **coupon_form)
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=f"Something wrong happened."
+    )
