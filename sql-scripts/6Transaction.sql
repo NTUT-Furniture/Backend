52f76b052f41189551db48f6e83e2249463a4c10
df <dragonflybaby2.0@gmail.com>
2023-12-25 19:21:04 +0800

    Updata DB schema

---
+++ sql-scripts/6Transaction.sql
@@ -0,0 +1,34 @@
+create table Transaction
+(
+    transaction_uuid varchar(64)                         not null
+        primary key,
+    account_uuid     varchar(64)                         not null,
+    coupon_code      varchar(16)                         null,
+    receive_time     datetime                            null,
+    status           varchar(32)                         null,
+    order_time       timestamp default CURRENT_TIMESTAMP not null,
+    update_time      timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    constraint Transaction_ibfk_2
+        foreign key (account_uuid) references Account (account_uuid),
+    constraint Transaction_status_check
+        check (status in ('Ordered', 'Delivering', 'Arrived', 'Cancelled')),
+    constraint Receive_after_arrive_check
+        check (receive_time is null or status = 'Arrived')
+);
+DELIMITER //
+
+CREATE TRIGGER check_transaction_coupon_code
+    BEFORE INSERT
+    ON Transaction
+    FOR EACH ROW
+BEGIN
+    IF NEW.coupon_code IS NOT NULL THEN
+        IF NOT EXISTS (SELECT 1 FROM Coupon WHERE coupon_code = NEW.coupon_code) THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'Invalid coupon code';
+        END IF;
+    END IF;
+END;
+//
+
+DELIMITER ;
--- sql-scripts/9events.sql
+++
@@ -1,4 +0,0 @@
-CREATE EVENT delete_expired_rows
-    ON SCHEDULE EVERY 1 DAY
-    DO
-    DELETE FROM Coupon WHERE expire_time < NOW();
