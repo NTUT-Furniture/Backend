56ffa975a2f56727b7290273551b4e4e1c3b4989
df <dragonflybaby2.0@gmail.com>
2023-12-25 21:06:25 +0800

    Add constraint that a transaction can only have products from one shop

--- sql-scripts/7TransactionProductLog.sql
+++ sql-scripts/7TransactionProductLog.sql
@@ -8,3 +8,26 @@ create table TransactionProductLog
     constraint TransactionProductLog_ibfk_2
         foreign key (product_uuid) references Product (product_uuid)
 );
+
+
+CREATE TRIGGER check_if_product_came_from_same_shop
+    BEFORE INSERT
+    ON TransactionProductLog
+    FOR EACH ROW
+BEGIN
+    IF NOT EXISTS (SELECT 1
+                   FROM TransactionProductLog,
+                        Product
+                   WHERE TransactionProductLog.transaction_uuid = NEW.transaction_uuid
+                     and TransactionProductLog.product_uuid = Product.product_uuid
+                     and Product.shop_uuid = (SELECT shop_uuid
+                                              FROM Product
+                                              WHERE product_uuid = NEW.product_uuid))
+        AND (SELECT 1
+             FROM TransactionProductLog
+             WHERE transaction_uuid = NEW.transaction_uuid)
+    THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'This product does not belong to the same shop as the transaction';
+    END IF;
+END;
