5b84ade495f504ec0e866437964845e25ffbbca5
df <dragonflybaby2.0@gmail.com>
2023-12-25 20:35:46 +0800

    Enhance Transaction model and introduce update endpoint
    Updated the Transaction model to incorporate an enum for transaction status. Added a PUT endpoint to allow updating transaction details such as receive time and status. Implemented checks based on user roles and permissions while updating transactions.

--- app/model/transaction.py
+++ app/model/transaction.py
@@ -1,6 +1,13 @@
 import datetime
+from enum import Enum
 
 from pydantic import BaseModel
+
+class TransactionStatusEnum(str, Enum):
+    Ordered = "Ordered"
+    Delivering = "Delivering"
+    Arrived = "Arrived"
+    Cancelled = "Cancelled"
 
 class TransactionProductLog(BaseModel):
     product_uuid: str
@@ -14,7 +21,7 @@ class Transaction(BaseModel):
     account_uuid: str
     coupon_code: str | None = None
     receive_time: datetime.datetime | None = None
-    status: str | None = None
+    status: TransactionStatusEnum
     order_time: datetime.datetime | None = None
     products: TransactionProductLogList
 
@@ -22,8 +29,12 @@ class TransactionCreate(BaseModel):
     account_uuid: str | None = None
     coupon_code: str | None = None
     receive_time: datetime.datetime | None = None
-    status: str
+    status: TransactionStatusEnum
     products: TransactionProductLogList
+
+class TransactionUpdate(BaseModel):
+    receive_time: datetime.datetime | None = None
+    status: TransactionStatusEnum | None = None
 
 class TransactionList(BaseModel):
     transactions: list[Transaction]
