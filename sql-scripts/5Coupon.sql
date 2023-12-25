674c0d4e373c176e8c0513bc5105c59339c6ae29
df <dragonflybaby2.0@gmail.com>
2023-12-25 20:04:17 +0800

    Add some constraints to Coupon

--- sql-scripts/5Coupon.sql
+++ sql-scripts/5Coupon.sql
@@ -1,8 +1,46 @@
 create table Coupon
 (
-    coupon_uuid varchar(64) not null primary key,
-    coupon_code varchar(16) not null,
+    coupon_uuid varchar(64)                         not null primary key,
+    coupon_code varchar(16)                         not null,
     discount    int                                 not null,
-    expire_time date                                not null,
-    update_time timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP
+    expire_time timestamp                           not null,
+    update_time timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    check (discount >= 0 and discount <= 100)
 );
+
+
+DELIMITER //
+
+CREATE TRIGGER check_coupon_code
+    BEFORE INSERT
+    ON Coupon
+    FOR EACH ROW
+BEGIN
+    IF NEW.coupon_code IS NOT NULL THEN
+        IF EXISTS (SELECT 1
+                   FROM Coupon
+                   WHERE coupon_code = NEW.coupon_code
+                     and expire_time >= convert_tz(now(), 'UTC', 'Asia/Taipei')) THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'There is a same coupon code that is not expired yet.';
+        END IF;
+    END IF;
+END;
+//
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER check_coupon_expire_time
+    BEFORE INSERT
+    ON Coupon
+    FOR EACH ROW
+BEGIN
+    IF NEW.coupon_code IS NOT NULL THEN
+        IF New.expire_time < convert_tz(now(), 'UTC', 'Asia/Taipei') THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'The coupon has already expired.';
+        END IF;
+    END IF;
+END;
+//
+DELIMITER ;
