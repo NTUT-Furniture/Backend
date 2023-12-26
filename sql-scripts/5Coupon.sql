cf3271c49b8e81eddc9cc745165f784fc818e248
DF <dragonflybaby2.0@gmail.com>
2023-12-27 05:45:10 +0800

    Add some tuples into table Coupon's init script

--- sql-scripts/5Coupon.sql
+++ sql-scripts/5Coupon.sql
@@ -44,3 +44,17 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('abc123', 'SAVE20', 20, '2024-12-31 23:59:59', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('abc890', 'NEWYEAR50', 50, '2024-12-31 23:59:59', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('def123', 'MEMORIAL20', 20, '2024-05-31 18:30:00', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('def456', 'GET50OFF', 50, '2024-06-30 18:30:00', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('ghi456', 'LABORDAY15', 15, '2024-09-30 12:00:00', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('ghi789', 'FREESHIP', 100, '2024-12-15 12:00:00', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('jkl012', 'HOLIDAY25', 25, '2024-12-25 23:59:59', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('jkl789', 'EASTER10', 10, '2024-04-15 23:59:59', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('mno345', 'BIGSALE', 30, '2024-07-31 18:30:00', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('pqr678', 'WINTERSALE', 40, '2024-08-28 12:00:00', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('stu901', 'SUMMERDEAL', 15, '2024-08-31 23:59:59', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('vwx234', 'FLASHSALE', 50, '2024-12-10 18:30:00', '2023-12-26 21:40:48');
+INSERT INTO NFT.Coupon (coupon_uuid, coupon_code, discount, expire_time, update_time) VALUES ('yzu567', 'SPRINGSALE', 35, '2024-04-30 12:00:00', '2023-12-26 21:40:48');
