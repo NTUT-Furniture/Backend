666d0d6b9835e3039744d44624ab8d536692fe1f
vincent <hankggg2003xd@gmail.com>
2023-12-26 17:43:53 +0800

    update transaction and transactionProductLog table

--- sql-scripts/6Transaction.sql
+++ sql-scripts/6Transaction.sql
@@ -3,7 +3,7 @@ create table Transaction
     transaction_uuid varchar(64)                         not null
         primary key,
     account_uuid     varchar(64)                         not null,
-    shop_uuid varchar(64) not null,
+    shop_uuid        varchar(64)                         not null,
     coupon_code      varchar(16)                         null,
     receive_time     datetime                            null,
     status           varchar(32)                         null,
@@ -13,68 +13,11 @@ create table Transaction
         foreign key (account_uuid) references Account (account_uuid),
     constraint Transaction_ibfk_3
         foreign key (shop_uuid) references Shop (shop_uuid),
-    constraint Transaction_status_check
-        check (status in ('Ordered', 'Delivering', 'Arrived', 'Cancelled')),
     constraint Receive_after_arrive_check
-        check (receive_time is null or status = 'Arrived')
+        check ((`receive_time` is null) or (`status` = _utf8mb4\'Arrived\')),
+	constraint Transaction_status_check
+		check (`status` in (_utf8mb4\'Ordered\',_utf8mb4\'Delivering\',_utf8mb4\'Arrived\',_utf8mb4\'Cancelled\'))
 );
-DELIMITER //
 
-CREATE TRIGGER check_transaction_coupon_code
-    BEFORE INSERT
-    ON Transaction
-    FOR EACH ROW
-BEGIN
-    IF NEW.coupon_code IS NOT NULL THEN
-        IF NOT EXISTS (SELECT 1
-                       FROM Coupon
-                       WHERE coupon_code = NEW.coupon_code
-                         and Coupon.expire_time >= convert_tz(now(), 'UTC', 'Asia/Taipei')) THEN
-            SIGNAL SQLSTATE '45000'
-                SET MESSAGE_TEXT = 'Invalid coupon code';
-        END IF;
-    END IF;
-END;
-//
-
-DELIMITER ;
-DELIMITER //
-
-CREATE TRIGGER check_transaction_coupon_code_on_update
-    BEFORE UPDATE
-    ON Transaction
-    FOR EACH ROW
-BEGIN
-    IF NEW.coupon_code IS NOT NULL THEN
-        IF NOT EXISTS (SELECT 1
-                       FROM Coupon
-                       WHERE coupon_code = NEW.coupon_code
-                         and Coupon.expire_time >= convert_tz(now(), 'UTC', 'Asia/Taipei')) THEN
-            SIGNAL SQLSTATE '45000'
-                SET MESSAGE_TEXT = 'Invalid coupon code';
-        END IF;
-    END IF;
-END;
-//
-
-DELIMITER ;
-
-
-INSERT INTO NFT.Transaction (transaction_uuid, account_uuid, coupon_code, receive_time, status, order_time, update_time)
-VALUES ('3dbb0f4f-7e4b-4de4-beed-1179dd9241d0', '831cd0d0-8916-11ee-bb92-42010aae0002', null, null, 'Cancelled',
-        '2023-12-25 12:33:38', '2023-12-25 13:38:59');
-INSERT INTO NFT.Transaction (transaction_uuid, account_uuid, coupon_code, receive_time, status, order_time, update_time)
-VALUES ('7ae4c64d-17ac-452d-a965-a52b4b948e91', '831cdf1e-8916-11ee-bb92-42010aae0002', null, null, 'Ordered',
-        '2023-12-25 13:36:10', '2023-12-25 13:36:10');
-INSERT INTO NFT.Transaction (transaction_uuid, account_uuid, coupon_code, receive_time, status, order_time, update_time)
-VALUES ('87db6a4b-d9a5-4313-aef6-b0ed6cacd7f0', '831cdf1e-8916-11ee-bb92-42010aae0002', null, null, 'Ordered',
-        '2023-12-25 13:33:46', '2023-12-25 13:33:46');
-INSERT INTO NFT.Transaction (transaction_uuid, account_uuid, coupon_code, receive_time, status, order_time, update_time)
-VALUES ('d5736bee-70c1-4f52-a767-a83a8ec4156b', '831cdf1e-8916-11ee-bb92-42010aae0002', null, null, 'Ordered',
-        '2023-12-25 13:35:53', '2023-12-25 13:35:53');
-INSERT INTO NFT.Transaction (transaction_uuid, account_uuid, coupon_code, receive_time, status, order_time, update_time)
-VALUES ('e96af1b3-2407-490d-9930-c76a57fdb97a', '831cdf1e-8916-11ee-bb92-42010aae0002', null, null, 'Ordered',
-        '2023-12-25 13:37:27', '2023-12-25 13:37:27');
-INSERT INTO NFT.Transaction (transaction_uuid, account_uuid, coupon_code, receive_time, status, order_time, update_time)
-VALUES ('f9e9ab39-869d-4d0a-adc2-11a148349b0d', '831cdf1e-8916-11ee-bb92-42010aae0002', null, null, 'Ordered',
-        '2023-12-25 13:31:49', '2023-12-25 13:31:49');
+INSERT INTO NFT.Transaction (transaction_uuid, account_uuid, shop_uuid, coupon_code, receive_time, status, order_time, update_time) VALUES ('325ea530-8f2f-11ee-8b4f-42010aae0002', '831cde2e-8916-11ee-bb92-42010aae0002', 'f150b186-8998-11ee-bb92-42010aae0002', null, '2023-12-26 17:20:47', 'Arrived', '2023-12-26 09:21:01', '2023-12-26 09:29:24');
+INSERT INTO NFT.Transaction (transaction_uuid, account_uuid, shop_uuid, coupon_code, receive_time, status, order_time, update_time) VALUES ('e31b3d02-2995-4e2c-9481-93a63403c7af', '4001309d-8f2f-11ee-8b4f-42010aae0002', 'c4f8d1b0-8d24-11ee-bb92-42010aae0002', null, null, 'Ordered', '2023-12-26 09:18:58', '2023-12-26 09:28:38');
--- sql-scripts/7TransactionProductLog.sql
+++ sql-scripts/7TransactionProductLog.sql
@@ -9,36 +9,6 @@ create table TransactionProductLog
         foreign key (product_uuid) references Product (product_uuid)
 );
 
-
-CREATE TRIGGER check_if_product_came_from_same_shop
-    BEFORE INSERT
-    ON TransactionProductLog
-    FOR EACH ROW
-BEGIN
-    IF NOT EXISTS (SELECT 1
-                   FROM TransactionProductLog,
-                        Product
-                   WHERE TransactionProductLog.transaction_uuid = NEW.transaction_uuid
-                     and TransactionProductLog.product_uuid = Product.product_uuid
-                     and Product.shop_uuid = (SELECT shop_uuid
-                                              FROM Product
-                                              WHERE product_uuid = NEW.product_uuid))
-        AND (SELECT 1
-             FROM TransactionProductLog
-             WHERE transaction_uuid = NEW.transaction_uuid)
-    THEN
-        SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'This product does not belong to the same shop as the transaction';
-    END IF;
-END;
-
-INSERT INTO NFT.TransactionProductLog (transaction_uuid, product_uuid, quantity)
-VALUES ('3dbb0f4f-7e4b-4de4-beed-1179dd9241d0', '4d567890-8998-11ee-bb92-42010aae0002', 213);
-INSERT INTO NFT.TransactionProductLog (transaction_uuid, product_uuid, quantity)
-VALUES ('3dbb0f4f-7e4b-4de4-beed-1179dd9241d0', '9802e81f-eabb-43d0-8317-79ff9e1fdda8', 213);
-INSERT INTO NFT.TransactionProductLog (transaction_uuid, product_uuid, quantity)
-VALUES ('d5736bee-70c1-4f52-a767-a83a8ec4156b', '094527b3-f8f1-4dfc-82cb-066a48d29caa', 0);
-INSERT INTO NFT.TransactionProductLog (transaction_uuid, product_uuid, quantity)
-VALUES ('7ae4c64d-17ac-452d-a965-a52b4b948e91', '094527b3-f8f1-4dfc-82cb-066a48d29caa', 0);
-INSERT INTO NFT.TransactionProductLog (transaction_uuid, product_uuid, quantity)
-VALUES ('e96af1b3-2407-490d-9930-c76a57fdb97a', '1a234567-8998-11ee-bb92-42010aae0002', 0);
+INSERT INTO NFT.TransactionProductLog (transaction_uuid, product_uuid, quantity) VALUES ('e31b3d02-2995-4e2c-9481-93a63403c7af', '5e678901-8998-11ee-bb92-42010aae0002', 23);
+INSERT INTO NFT.TransactionProductLog (transaction_uuid, product_uuid, quantity) VALUES ('e31b3d02-2995-4e2c-9481-93a63403c7af', '6f789012-8998-11ee-bb92-42010aae0002', 213121);
+INSERT INTO NFT.TransactionProductLog (transaction_uuid, product_uuid, quantity) VALUES ('325ea530-8f2f-11ee-8b4f-42010aae0002', '094527b3-f8f1-4dfc-82cb-066a48d29caa', 34);
