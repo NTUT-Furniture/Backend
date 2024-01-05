ffdfa60b9d56db0dba7014e17ad858ac1e3e52e2
vincent <hankggg2003xd@gmail.com>
2024-01-06 00:03:02 +0800

    remove end of line

--- wait.sh
+++ wait.sh
@@ -1,4 +1,4 @@
-until nc -z -v -w30 "nftbackend-mysql-1.nftbackend_default" 3306
+until nc -z -v -w30 "mysql" 3306
 do
   echo "Waiting a second until the database is receiving connections..."
   # wait for a second before checking again
